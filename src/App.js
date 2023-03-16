02e15bc5403739f19d00d0349e7dcbb7f895bdbc
Mawulolo Ameko <mawulolo.ameko@gmail.com>
2023-03-16 10:41:27 -0400

    add image link form

--- src/App.js
+++ src/App.js
@@ -7,13 +7,31 @@ import Rank from "./components/Rank/Rank";
 import Particle from "./components/Particle/Particle";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         <Particle className="particles" />
         {/*
         
