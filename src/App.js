7477a69dd593c79d63c93b780e93c387f3af0ce8
Mawulolo Ameko <mawulolo.ameko@gmail.com>
2023-03-27 10:11:31 -0400

    signin and routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Rank from "./components/Rank/Rank";
 import Particle from "./components/Particle/Particle";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 
 class App extends Component {
   constructor() {
@@ -14,22 +16,10 @@ class App extends Component {
       input: "",
       imageUrl: "",
       box: {},
+      route: "signin",
+      isSignedIn: false,
     };
   }
-
-  // calculateFaceLocation = (data) => {
-  //   const clarifaiFace =
-  //     data.outputs[0].data.regions[0].region_info.bounding_box;
-  //   const image = document.getElementById("inputimage");
-  //   const width = Number(image.width);
-  //   const height = Number(image.height);
-  //   return {
-  //     leftCol: clarifaiFace.left_col * width,
-  //     topRow: clarifaiFace.top_row * height,
-  //     rightCol: width - clarifaiFace.right_col * width,
-  //     bottomRow: height - clarifaiFace.bottom_row * height,
-  //   };
-  // };
 
   calculateFaceLocation = (data) => {
     const clarifaiFace =
@@ -46,7 +36,6 @@ class App extends Component {
   };
 
   displayFaceBox = (box) => {
-    console.log(box);
     this.setState({ box });
   };
 
@@ -92,18 +81,40 @@ class App extends Component {
       .catch((error) => console.log("error", error));
   };
 
+  onRouteChange = (route) => {
+    if (route === "signout") {
+      this.setState({ isSignedIn: false });
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-        <Particle className="particles" />
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        {route === "home" ? (
+          <div>
+            {" "}
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <Particle className="particles" />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
