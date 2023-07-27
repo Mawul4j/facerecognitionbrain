dd604627b01c43d6766ab638a7537c4cbc159990
Mawulolo Ameko <mawulolo.ameko@gmail.com>
2023-07-27 11:20:44 -0400

    updaing scripts with loadUser feature

--- src/App.js
+++ src/App.js
@@ -123,7 +123,10 @@ class App extends Component {
           <div>
             {" "}
             <Logo />
-            <Rank />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -132,7 +135,7 @@ class App extends Component {
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
         ) : route === "signin" ? (
-          <Signin onRouteChange={this.onRouteChange} />
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
           <Register
             loadUser={this.loadUser}
